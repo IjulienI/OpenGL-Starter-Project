f607f4d00850e831569ad01eaa1dc1858cb2d2cf
Julien ssm <89165787+IjulienI@users.noreply.github.com>
2024-11-05 12:10:05 +0100

    dvddddd !!!

--- GLP/OpenGL-Starter/SDL-0/Main.cpp
+++ GLP/OpenGL-Starter/SDL-0/Main.cpp
@@ -8,6 +8,8 @@ using namespace std;
 
 string LoadShader(string fileName);
 
+void randomColor(unsigned int shaderProgram);
+
 int main(int argc, char* argv[])
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
@@ -41,7 +43,7 @@ int main(int argc, char* argv[])
 	glViewport(0,0, width, height);
 	
 	//Put the color you want here for the background
-	glClearColor(0.0f, 0.0f, 0.2f, 1.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
 	string vertexFile = LoadShader("simpleVertex.glsl");
 	const char *vertexShaderSource = vertexFile.c_str();
@@ -49,11 +51,32 @@ int main(int argc, char* argv[])
 	string fragmentFile = LoadShader("simpleFragment.glsl");
 	const char *fragmentShaderSource = fragmentFile.c_str();
 
+	string vertexFile1 = LoadShader("BaseVertex.glsl");
+	const char *fixVertexShaderSource = vertexFile1.c_str();
+
+	string fragmentFile1 = LoadShader("BaseFragment.glsl");
+	const char *fixFragmentShaderSource = fragmentFile1.c_str();
+
 	//Describe the shape by its vertices
 	float vertices[]={
-		0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   
-	   -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   
-		0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f
+		// losange
+		0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
+	   -0.5f, 0.0f, 0.0f,  0.0f, 1.0f, 0.0f,   		
+		0.5f, 0.0f, 0.0f,  1.0f, 0.0f, 0.0f, 
+		0.0f, -0.5f, 0.0f,	1.0f, 0.0f, 0.0f,
+		//Crown
+		//Strip
+		-0.4f, 0.2f, 0.0f, 0.0f, 0.0f, 1.0f,
+		-0.4f, -0.2f, 0.0f, 0.1f, 0.0f, 0.0f,
+		-0.2f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, -0.2f, 0.0f, 0.0f, 0.0f, 1.0f,
+		 0.2f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+		0.4f, -0.2f, 0.0f, 0.0f, 0.0f, 1.0f,
+		0.4f, 0.2f, 0.0f, 0.0f, 0.0f, 1.0f,
+		//SimpleTriangle
+		-0.2f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+		0.0f, 0.2f, 0.0f,  0.0f, 0.0f, 1.0f,
+		0.2f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f		
 	};
 	
 	//Create an ID to be given at object generation
@@ -76,8 +99,24 @@ int main(int argc, char* argv[])
 	glShaderSource(fragmentShaderID, 1, &fragmentShaderSource, nullptr);
 	glCompileShader(fragmentShaderID);
 
+	unsigned int fixVertexShaderID;
+	fixVertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(fixVertexShaderID, 1, &fixVertexShaderSource, nullptr);
+	glCompileShader(fixVertexShaderID);
+
+	unsigned int fixFragmentShaderID;
+	fixFragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fixFragmentShaderID, 1, &fixFragmentShaderSource, nullptr);
+	glCompileShader(fixFragmentShaderID);
+
 	unsigned int shaderProgram;
 	shaderProgram = glCreateProgram();
+
+	unsigned int fixShaderProgram;
+	fixShaderProgram = glCreateProgram();
+
+	glAttachShader(fixShaderProgram, fixVertexShaderID);
+	glAttachShader(fixShaderProgram, fixFragmentShaderID);
 
 	//attach shaders to use into the program
 	glAttachShader(shaderProgram, vertexShaderID);
@@ -85,6 +124,7 @@ int main(int argc, char* argv[])
 
 	//link the program shader
 	glLinkProgram(shaderProgram);
+	glLinkProgram(fixShaderProgram);
 
 	//use the program
 	glUseProgram(shaderProgram);
@@ -113,6 +153,14 @@ int main(int argc, char* argv[])
 
 	//0 is our origin, the higher the z, the farther the object
 	glDepthFunc(GL_LESS);
+	
+	float posX = 0.0f;
+	float posY = 0.0f;
+	bool left = false;
+	bool up = true;
+
+	float lastDeltaTime = 0.0f;
+	float deltaTime = 0.0f;
 
 	bool isRunning = true;
 	while(isRunning)
@@ -130,21 +178,62 @@ int main(int argc, char* argv[])
 		}
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the screen
 
-		float speed = 2.0f;
-		float timeValue = ((float)SDL_GetTicks()/1000) * speed;
-		float trianglePosX = (sin(timeValue * speed) / 2.0f );
-		float triangleSizeX = (sin(timeValue * speed) / 2.0f );
-		float triangleSizeY = (cos(timeValue * speed) + 2 / 2.0f );
-		float triangleSizeZ = (sin(timeValue * speed) + 4 / 2.0f );
-		int vertexLocation = glGetUniformLocation(shaderProgram, "xOffset");
-		int vertexSize = glGetUniformLocation(shaderProgram, "size");
-		glUniform3f(vertexSize,triangleSizeX,triangleSizeY,triangleSizeZ );
-		glUniform1f(vertexLocation, trianglePosX);
+		// float speed = 2.0f;
+		// float timeValue = ((float)SDL_GetTicks()/1000) * speed;
+		// float trianglePosX = (sin(timeValue * speed) / 2.0f );
+		// float triangleSizeX = (cos(timeValue * speed) / 2.0f );
+		// float triangleSizeY = (cos(timeValue * speed) + 2 / 2.0f );
+		// float triangleSizeZ = (sin(timeValue * speed) + 4 / 2.0f );
+		// int vertexLocation = glGetUniformLocation(shaderProgram, "xOffset");
+		// int vertexSize = glGetUniformLocation(shaderProgram, "size");
+		// glUseProgram(shaderProgram);		
+		// glUniform3f(vertexSize,triangleSizeX,triangleSizeY,triangleSizeZ );
+		// glUniform1f(vertexLocation, trianglePosX);
+
 		glUseProgram(shaderProgram);
+		float speed = 0.5f;
+		float tickTime = (float)SDL_GetTicks()/1000;
+		deltaTime = tickTime - lastDeltaTime;
+		lastDeltaTime = tickTime;
+		
+		if(!left)posX += speed * deltaTime;
+		else posX -= speed * deltaTime;
+
+		if(up) posY += speed * deltaTime;
+		else posY -= speed * deltaTime;
+
+		if(posX >= 0.8f)
+		{
+			left = true;
+			randomColor(shaderProgram);
+		}
+		else if(posX <= -0.8)
+		{
+			left = false;
+			randomColor(shaderProgram);
+		}
+
+		if(posY >= 0.9f)
+		{
+			up = false;
+			randomColor(shaderProgram);
+		}
+		else if(posY <= -0.9f)
+		{
+			up = true;
+			randomColor(shaderProgram);
+		}
+		
+		int vertexLocation = glGetUniformLocation(shaderProgram, "offsets");
+		glUniform2f(vertexLocation, posX, posY);
 
 		//OMG WE FINALLY DRAW ! We use the GL_TRIANGLES primitive type
-		//We draw from vertex 0 and we will be drawing 3 vertices
-		glDrawArrays(GL_TRIANGLES, 0, 3);		
+		//We draw from vertex 0 and we will be drawing 3 vertices			
+		glDrawArrays(GL_TRIANGLE_STRIP, 4, 7);
+		glDrawArrays(GL_TRIANGLES, 11,3);
+		
+		//glUseProgram(fixShaderProgram);
+		//glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 		
 		SDL_GL_SwapWindow(Window); // Swapbuffer
 	}
@@ -170,4 +259,13 @@ string LoadShader(string fileName) {
   
 	myFile.close();
 	return fileText;
+}
+
+void randomColor(unsigned int shaderProgram)
+{
+	int shaderColor = glGetUniformLocation(shaderProgram, "color");
+	float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+	float g = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+	float b = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+	glUniform3f(shaderColor,r ,g ,b);
 }
--- GLP/OpenGL-Starter/SDL-0/simpleVertex.glsl
+++ GLP/OpenGL-Starter/SDL-0/simpleVertex.glsl
@@ -3,14 +3,15 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor; 
   
-//out vec3 ourColor; 
+uniform vec3 color = vec3(1,1,1);
+out vec3 ourColor; 
 out vec3 ourPosition;
-uniform vec3 size = vec3(0.5,0.2,1.0);
-uniform float xOffset;
+uniform vec3 size = vec3(0.5,0.5,0.5);
+uniform vec2 offsets;
 
 void main()
 {
-    ourPosition = vec3((aPos.x * size.x) + xOffset, aPos.y * size.y, aPos.z * size.z);
+    ourPosition = vec3((aPos.x * size.x) + offsets.x, (aPos.y * size.y) + offsets.y, aPos.z * size.z);
     gl_Position = vec4(ourPosition, 1.0);
-    //ourColor = aColor;    
+    ourColor = color;    
 }       
